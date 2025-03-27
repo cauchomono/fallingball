8d9ea228d08ec61fce1e379384bb8f8b4b2e0471
cauchomono <83195469+cauchomono@users.noreply.github.com>
2025-03-27 16:43:11 -0500

    random obstacles

--- spawner.gd
+++ spawner.gd
@@ -1,38 +1,40 @@
 extends Node2D
 
-@export var obstacle_scene: PackedScene  # Arrastra aquí la escena del obstáculo
-@export var spawn_rate: float = 1.5  # Tiempo entre cada obstáculo generado
-@export var wall_distance: Vector2 = Vector2(0, 10)  # Rango de aparición en X
+@export var random_obstacle_scene: Array[PackedScene]
+@export var obstacle_scene: PackedScene
+@export var spawn_rate: float = 1.5
+@export var wall_distance: Vector2 = Vector2(0, 10)
 var obstacle_size : Vector2
+var obstacle_index : int
 func _ready():
-	spawn_obstacle()
+	spawn_random_obstacle()
 	$Timer.wait_time = spawn_rate
 	$Timer.start()
-	obstacle_size = get_obstacle_size()
+	obstacle_size = get_random_obstacle_size()
 
-func spawn_obstacle():
-	print("Inicia spawn de objeto")
-	var obstacle = obstacle_scene.instantiate()
+
+func spawn_random_obstacle():
+	obstacle_index  = randi_range(0,random_obstacle_scene.size()-1)
+	var obstacle = random_obstacle_scene[obstacle_index].instantiate()
 	add_child(obstacle)
-	# Posición inicial aleatoria dentro del rango
 	
 	var x_spawn_range : Vector2 = Vector2(wall_distance.x + obstacle_size.x,wall_distance.y - obstacle_size.x)
 	var spawn_x = randf_range(x_spawn_range.x , x_spawn_range.y)
 	obstacle.global_position = Vector2(spawn_x, position.y)
 	
 func _on_timer_timeout():
-	spawn_obstacle()
+	spawn_random_obstacle()
 	
-func get_obstacle_size():
-	if not obstacle_scene:
-		return Vector2.ZERO  # Retorna tamaño 0 si no hay escena asignada
+func get_random_obstacle_size():
+	if not random_obstacle_scene[obstacle_index]:
+		return Vector2.ZERO
 
-	var obstacle_instance = obstacle_scene.instantiate()  # Instancia la escena
-	var collider = obstacle_instance.get_node_or_null("CollisionShape2D")  # Busca el collider
+	var obstacle_instance : Node = random_obstacle_scene[obstacle_index].instantiate()
+	var collider : CollisionShape2D = obstacle_instance.get_node_or_null("CollisionShape2D")
 
 	if collider and collider.shape is RectangleShape2D:
-		return collider.shape.size  # Retorna el tamaño del rectángulo
+		return collider.shape.size
 	elif collider and collider.shape is CapsuleShape2D:
-		return Vector2(collider.shape.radius * 2, collider.shape.height)  # Ancho y alto de cápsula
+		return Vector2(collider.shape.radius * 2, collider.shape.height)
 
-	return Vector2.ZERO  # Si no tiene collider, retorna tamaño 0
+	return Vector2.ZERO
