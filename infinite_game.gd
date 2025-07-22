6196ec4f55ae7f866b5f9a19ea4858f4c14ef4ac
cauchomono <83195469+cauchomono@users.noreply.github.com>
2025-07-22 17:37:05 -0500

    add menu music and gameplay

--- infinite_game.gd
+++ infinite_game.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
+@onready var audioStreamPlayer : AudioStreamPlayer = $AudioStreamPlayer
 var score = 0
+var gameplaySound = preload("res://sounds/music/Ludum Dare 28 01.ogg")
 
 func _ready():
+	
+	audioStreamPlayer.stream = gameplaySound
+	audioStreamPlayer.play()
 	Globals.points = score
 	$PointsZone.enemy_passed.connect(_on_enemy_passed) 
 	$Spawner.obstacle_hit.connect(_on_player_touch)
--- main_menu.gd
+++ main_menu.gd
@@ -1,5 +1,11 @@
 extends Control
 
+@onready var audioStreamPlayer : AudioStreamPlayer = $AudioStreamPlayer
+var menuMusic = preload("res://sounds/music/Ludum Dare 28 04.ogg")
 
+func _ready():
+	audioStreamPlayer.stream = menuMusic
+	audioStreamPlayer.play()
+	
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://InfiniteGame.tscn")
