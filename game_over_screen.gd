253b70ec88ee9efdf42d8b6accd97830529b6bb8
cauchomono <83195469+cauchomono@users.noreply.github.com>
2025-07-27 17:58:12 -0500

    fix bug

--- game_over_screen.gd
+++ game_over_screen.gd
@@ -3,6 +3,8 @@ extends CanvasLayer
 func _ready() -> void:
 	%GameOverScore.text =  "Your score: " + str(Globals.points)
 
+func _process(delta: float) -> void:
+	%GameOverScore.text =  "Your score: " + str(Globals.points)
 
 func _on_main_menu_page_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu.tscn")
