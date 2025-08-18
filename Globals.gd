150069e4c2b63954f6a11411d8024f894d77c64b
cauchomono <83195469+cauchomono@users.noreply.github.com>
2025-08-18 00:05:54 -0500

    Add bet points functionality to bet screen
    Introduces a new 'bet_points' variable in Globals.gd to track bet amounts. Updates bet_screen.gd and bet_screen.tscn to allow users to set bet points via a slider, and adds Bet and Main Menu buttons with corresponding logic and scene connections.

--- Globals.gd
+++ Globals.gd
@@ -2,4 +2,5 @@ extends Node
 
 var score : int = 0
 var level : int = 1
-var high_score
+var high_score : int = 0
+var bet_points : int = 0
--- bet_screen.gd
+++ bet_screen.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
-
+func _ready() -> void:
+	%PointsBet.text = str(int(%Slider.value))
 
 
 func _on_h_slider_value_changed(value: float) -> void:
 	%PointsBet.text = str(int(value))
-	print(value) # Replace with function body.
+	print(value)
+
+
+func _on_bet_pressed() -> void:
+	Globals.bet_points = int(%Slider.value)
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")# Replace with function body.
