f82408a93ce743bbe73cfbd4748685ac53d1bed1
cauchomono <83195469+cauchomono@users.noreply.github.com>
2025-10-07 02:20:36 -0500

    Add score multiplier logic to game over screen
    Introduced points_multiplier and select_multiplier functions to adjust the final score based on bet points. Removed max_value from bet_screen.tscn to allow flexible betting. The multiplier is now applied when the game over screen is shown.

--- game_over_screen.gd
+++ game_over_screen.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 func _ready() -> void:
+	points_multiplier(select_multiplier())
 	%GameOverScore.text =  "Your score: " + str(Globals.score)
 
 func _process(delta: float) -> void:
@@ -13,3 +14,17 @@ func _on_main_menu_page_pressed() -> void:
 func _on_restart_page_pressed() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+	
+func points_multiplier(multiplier:int ):
+	Globals.score *= multiplier
+
+func select_multiplier() -> float:
+	print("Se ejecuta select_multiplier")
+	if(Globals.score < Globals.bet_points):
+		return 0.5
+	if(Globals.bet_points > 5):
+		return 2
+	if(Globals.bet_points > 15):
+		return 5
+	return 1
+	
